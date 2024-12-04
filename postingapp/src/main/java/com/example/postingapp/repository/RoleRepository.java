abd0b97d7a92ca72c9567565e62b1b8121cbba05
ryunosuke-miyazaki <rymiyazaki@japanet.co.jp>
2024-12-04 17:58:40 +0900

    Delete postingapp directory

--- postingapp/src/main/java/com/example/postingapp/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.postingapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.postingapp.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role,Integer>{
-
-}
